8f5606e0fb6eb06e409007b381b09221067c0ba2
Maxence Bon <maxence.bon@orange.fr>
2022-12-23 17:43:38 +0100

    tp

--- controllers/app.controllers.js
+++ controllers/app.controllers.js
@@ -1,5 +1,4 @@
-const { resolve } = require('path')
-
+// const { resolve } = require('path')
 
 exports.homePage = (req,res) => {
     // res.sendFile(resolve('public', 'index.html'));   En html
@@ -9,18 +8,18 @@ exports.homePage = (req,res) => {
 
 exports.contactPage = (req,res) => {
     // res.sendFile(resolve('public', 'contact.html'));
-    res.send(require('../views/contact'))
+    // res.send(require('../views/contact'))
     res.render('pages/contact.pug')
 }
 
 exports.servicesPage = (req,res) => {
     // res.sendFile(resolve('public', 'services.html'));
-    res.send(require('../views/services'))
+    // res.send(require('../views/services'))
     res.render('pages/services.pug')
 }
 
 exports.usersPage = (req,res) => {
     // res.sendFile(resolve('public', 'utilisateurs.html'));
-    res.send(require('../views/pages/users'))
+    // res.send(require('../views/pages/users'))
     res.render('pages/users.pug')
 }
